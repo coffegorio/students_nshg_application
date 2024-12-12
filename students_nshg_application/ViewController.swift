73ecc561d1d919ea22f3370ea314abdab3f621f0
coffegorio <coffegorio@vk.com>
2024-12-13 01:38:34 +0300

    Initial Commit

---
+++ students_nshg_application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  students_nshg_application
+//
+//  Created by Егорио on 13.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
